8dc13f24638955eef2b52b9daecc67953179314d
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2024-02-07 12:58:16 -0800

    CDHD: rendered monitoring_data reports for 2014:2023

---
+++ 2024/Chelan-Douglas_Health_District/R/render_monitoring_data_reports.R
@@ -0,0 +1,25 @@
+# Render the monitoring_data.Rmd report for successive years
+#
+#
+# NOTE:  Data archives begin in 2014
+
+# ----- Setup ------------------------------------------------------------------
+
+# Check that the working directory is set properly
+if ( !stringr::str_detect(getwd(), "Chelan-Douglas_Health_District$") ) {
+  stop("WD_ERROR:  Please set the working directory to 'Chelan-Douglas_Health_District/'")
+}
+
+
+for ( year in 2014:2023 ) {
+  
+  params <- list(year = year)
+  
+  # This path is relative to the Rmd/ directory
+  htmlPath <- sprintf("./html/monitoring_data_%d.html", year)
+  
+  rmarkdown::render(input = 'Rmd/monitoring_data.Rmd',
+                    params = params,
+                    output_file = htmlPath)
+  
+}
